95b2d66d431a65e871273eaa0b8a93b1ac6ad162
Alice Di Tucci <aliceditucc@gmail.com>
2023-04-27 12:39:48 +0200

    Scale feature and clear out comments from class

--- my_softmax.py
+++ my_softmax.py
@@ -3,12 +3,10 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.activations import linear, relu, sigmoid
-#%matplotlib widget
 import matplotlib.pyplot as plt
-#plt.style.use('./deeplearning.mplstyle')
 
-# UNQ_C1
-# GRADED CELL: my_softmax
+#for feature scaling (can be done also in tf right?)
+from sklearn.preprocessing import StandardScaler
 
 #build my softmax with numpy
 def my_softmax(z):  
@@ -41,33 +39,27 @@ my_softmax(z)
 #Automated handwritten digit recognition is widely used today - 
 # from recognizing zip codes (postal codes) on mail envelopes to recognizing amounts written on bank checks.
 
-# def load_data():
-#     X = np.load("data/X.npy")
-#     y = np.load("data/y.npy")
-#     return X, y
-# #capire come si fa a prendere dati da MINST
+#The data set contains 10000 training examples of handwritten digits.
+#Each training example is a 28-pixel x 28-pixel grayscale image of the digit.
+#Each pixel is represented by a floating-point number indicating the grayscale intensity at that location.
+#The 28 by 28 grid of pixels is “unrolled” into a 784-dimensional vector.
+#Each training examples becomes a single row in our data matrix X.
+#This gives us a 10000 x 784 matrix X where every row is a training example of a handwritten digit image.
+#The second part of the training set is a 10000 x 1 dimensional vector y that contains labels for the training set
+#y = 0 if the image is of the digit 0, y = 4 if the image is of the digit 4 and so on.
 
-# # load dataset
-# X, y = load_data()
-
-# #The data set contains 5000 training examples of handwritten digits.
-# #Each training example is a 20-pixel x 20-pixel grayscale image of the digit.
-# #Each pixel is represented by a floating-point number indicating the grayscale intensity at that location.
-# #The 20 by 20 grid of pixels is “unrolled” into a 400-dimensional vector.
-# #Each training examples becomes a single row in our data matrix X.
-# #This gives us a 5000 x 400 matrix X where every row is a training example of a handwritten digit image.
-# #The second part of the training set is a 5000 x 1 dimensional vector y that contains labels for the training set
-# #y = 0 if the image is of the digit 0, y = 4 if the image is of the digit 4 and so on.
-
-#I dowload the data my way
+#Load data set
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(
     path='mnist.npz'
 )
 
+#Convert the 28x28 matrinc into a 784 array
+#Convert 1-D arrays into 2-D because the commands later will require it
+#y = np.expand_dims(y, axis=1)
 X = x_test.reshape(-1, 784)
 y = y_test.reshape(10000, -1)
 
-# #get more familiar with the data set
+#get more familiar with the data set
 
 #print some data elements
 print ('The first element of X is: ', X[0])
@@ -88,8 +80,6 @@ m, n = X.shape
 fig, axes = plt.subplots(8,8, figsize=(5,5))
 fig.tight_layout(pad=0.13,rect=[0, 0.03, 1, 0.91]) #[left, bottom, right, top]
 
-#fig.tight_layout(pad=0.5)
-#widgvis(fig)
 for i,ax in enumerate(axes.flat):
     # Select random indices
     random_index = np.random.randint(m)
@@ -106,18 +96,28 @@ for i,ax in enumerate(axes.flat):
     ax.set_axis_off()
     fig.suptitle("Label, image", fontsize=14)
 plt.show()
+
+#Feature scaling
+
+# Initialize the class
+scaler_linear = StandardScaler()
+
+# Compute the mean and standard deviation of the training set then transform it
+X_train_scaled = scaler_linear.fit_transform(X)
+
+#print some data elements
+print ('The first element of X is: ', X_train_scaled[0])
+
 #Model representation
 
 #The neural network you will use in this assignment has two dense layers with ReLU activations 
 #followed by an output layer with a linear activation.
 #Recall that our inputs are pixel values of digit images.
-#Since the images are of size  20×20, this gives us  400 inputs.
+#Since the images are of size  28×28, this gives us  784 inputs.
 #Softmax is applied separately at the end with the loss function. 
 #The parameters have dimensions that are sized for a neural network with 25 units in layer 1,  
 # 15 units in layer 2 and  10 output units in layer 3, one for each digit.
 
-# UNQ_C2
-# GRADED CELL: Sequential model
 tf.random.set_seed(1234) # for consistent results
 model = Sequential(
     [               
@@ -133,7 +133,7 @@ model.summary() #summary of the model
 
 [layer1, layer2, layer3] = model.layers
 
-#### Examine Weights shapes
+#Examine Weights shapes
 W1,b1 = layer1.get_weights()
 W2,b2 = layer2.get_weights()
 W3,b3 = layer3.get_weights()
@@ -149,7 +149,7 @@ model.compile(
 
 #train the model
 history = model.fit(
-    X,y,
+    X_train_scaled,y, #changed from X to X_train_scaled
     epochs=40
 )
 
@@ -162,7 +162,6 @@ history = model.fit(
 
 def plot_loss_tf(history):
     fig,ax = plt.subplots(1,1, figsize = (4,3))
-    #widgvis(fig)
     ax.plot(history.history['loss'], label='loss')
     ax.set_ylim([0, 2])
     ax.set_xlabel('Epoch')
@@ -179,7 +178,7 @@ plot_loss_tf(history)
 print(f'expected value for X[1015]: {y[1015]}')
 
 def display_digit(X):
-    """ display a single digit. The input is one digit (400,). """
+    """ display a single digit. The input is one digit (784,). """
     fig, ax = plt.subplots(1,1, figsize=(0.5,0.5))
     #widgvis(fig)
     X_reshaped = X.reshape((28,28))
@@ -190,30 +189,22 @@ def display_digit(X):
 image_of_four = X[1015]
 display_digit(image_of_four)
 
-prediction = model.predict(image_of_four.reshape(1,784))  # prediction
+image_of_four_rescaled= X_train_scaled[1015]
+prediction = model.predict(image_of_four_rescaled.reshape(1,784))  # prediction
 
-print(f" predicting a four: \n{prediction}")
-print(f" Largest Prediction index: {np.argmax(prediction)}")
-
-# The largest output is prediction[2], indicating the predicted digit is a '2'. 
+# The largest output is prediction[4], indicating the predicted digit is a '4'. 
 # If the problem only requires a selection, that is sufficient. Use NumPy argmax to select it. 
-# If the problem requires a probability, a softmax is required:
+# If the problem requires a probability, a softmax is required.
+print(f" Model output: \n{prediction}")
+print(f" Predicted number is: {np.argmax(prediction)}")
 
-prediction_p = tf.nn.softmax(prediction)
-
-print(f" predicting a four. Probability vector: \n{prediction_p}")
-print(f"Total of predictions: {np.sum(prediction_p):0.3f}")
-
-#np.argmax returns the index of the largest value, i.e. the predicted value (the one with highest probability)
-yhat = np.argmax(prediction_p)
-
-print(f"np.argmax(prediction_p): {yhat}")
 
 #Another example:
 test = X[1012]
 display_digit(test)
+test_rescaled = X_train_scaled[1012]
 
-prediction = model.predict(test.reshape(1,784))  # prediction
+prediction = model.predict(test_rescaled.reshape(1,784))  # prediction
 print(f"Predicted number is: {np.argmax(prediction)}")
 
 #Let's compare the predictions vs the labels for a random sample of 64 digits. This takes a moment to run
@@ -225,7 +216,6 @@ m, n = X.shape
 
 fig, axes = plt.subplots(8,8, figsize=(5,5))
 fig.tight_layout(pad=0.13,rect=[0, 0.03, 1, 0.91]) #[left, bottom, right, top]
-#widgvis(fig)
 for i,ax in enumerate(axes.flat):
     # Select random indices
     random_index = np.random.randint(m)
@@ -238,7 +228,7 @@ for i,ax in enumerate(axes.flat):
     ax.imshow(X_random_reshaped, cmap='gray')
     
     # Predict using the Neural Network
-    prediction = model.predict(X[random_index].reshape(1,784))
+    prediction = model.predict(X_train_scaled[random_index].reshape(1,784))
     prediction_p = tf.nn.softmax(prediction)
     yhat = np.argmax(prediction_p)
     
@@ -250,8 +240,8 @@ plt.show()
 
 #Let's look at some of the errors:
 
-def display_errors(model,X,y):
-    f = model.predict(X)
+def display_errors(model,X,y, X_train_scaled):
+    f = model.predict(X_train_scaled, verbose = 0)
     yhat = np.argmax(f, axis=1)
     doo = yhat != y[:,0]
     idxs = np.where(yhat != y[:,0])[0]
@@ -261,7 +251,6 @@ def display_errors(model,X,y):
         cnt = min(8, len(idxs))
         fig, ax = plt.subplots(1,cnt, figsize=(5,1.2))
         fig.tight_layout(pad=0.13,rect=[0, 0.03, 1, 0.80]) #[left, bottom, right, top]
-        #widgvis(fig)
 
         for i in range(cnt):
             j = idxs[i]
@@ -271,7 +260,7 @@ def display_errors(model,X,y):
             ax[i].imshow(X_reshaped, cmap='gray')
 
             # Predict using the Neural Network
-            prediction = model.predict(X[j].reshape(1,784))
+            prediction = model.predict(X_train_scaled[j].reshape(1,784), verbose=0)
             prediction_p = tf.nn.softmax(prediction)
             yhat = np.argmax(prediction_p)
 
@@ -281,7 +270,6 @@ def display_errors(model,X,y):
             fig.suptitle("Label, yhat", fontsize=12)
     return(len(idxs))
 
-
-print( f"{display_errors(model,X,y)} errors out of {len(X)} images")
-
-print('Isn\'t this too many errors?')
+tot_errors = display_errors(model,X,y,X_train_scaled)
+print( f"{tot_errors} errors out of {len(X)} images")
+print(f'Error ratio: {tot_errors/len(X)}')
